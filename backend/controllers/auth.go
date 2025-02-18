16bbc4c038e0064698cb196442ec8b9ed8991a01
Torbjørn Bratland <torbjorn.bratland@bouvet.no>
2025-02-18 09:26:52 +0100

    stuffs

--- backend/controllers/auth.go
+++ backend/controllers/auth.go
@@ -14,6 +14,7 @@ type RegisterInput struct {
 	Name     string `json:"name" binding:"required"`
 	Email    string `json:"email" binding:"required,email"`
 	Password string `json:"password" binding:"required,min=6"`
+	lastname string `json:"lastNAmeA" binding:"required"`
 }
 
 type LoginInput struct {
